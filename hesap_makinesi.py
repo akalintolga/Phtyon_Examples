c1fc725d5d9b1fd390ab7d6f516d738b94e487e1
akalintolga <tolgaakalin@yandex.com>
2021-09-01 20:15:32 +0300

    part1

---
+++ hesap_makinesi.py
@@ -0,0 +1,12 @@
+class hesapmakinesi (object):
+
+   def __init__(self,sayi1,sayi2):
+
+       self.sayi1 = self.sayi1
+       self.sayi2 = self.sayi2
+
+    
+    def topla(self):
+
+        return self.sayi1 + self.sayi2
+
