ba94b7f2043ce106047ef12762f4d81990ad5984
akalintolga <tolgaakalin@yandex.com>
2021-09-01 21:26:59 +0300

    part 2

--- hesap_makinesi.py
+++ hesap_makinesi.py
@@ -2,11 +2,30 @@ class hesapmakinesi (object):
 
    def __init__(self,sayi1,sayi2):
 
-       self.sayi1 = self.sayi1
-       self.sayi2 = self.sayi2
+       self.value1 = self.sayi1
+       self.value2 = self.sayi2
 
     
-    def topla(self):
+   def topla(self):
 
-        return self.sayi1 + self.sayi2
+        return self.value1 + self.value2
+
+   def cikar (self):
+
+       return self.sayi1 - self.sayi2
+
+   def carp (self):
+
+        return self.sayi1 * self.sayi2
+
+   def bol (self):
+
+       return self.sayi1 / self.sayi2
+
+
+
+sayilar1 = int(input("Birinci Sayıyı Giriniz: "))
+sayilar2 = int(input("İkinci Sayıyı Giriniz:  "))
+sonuc = hesapmakinesi(sayilar1,sayilar2)
+print("Girilen Sayıların Toplamı: ",sonuc.topla())
 
